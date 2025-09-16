19393110347c49be8cf48f2c044fdc1320fea7c8
Natzaum2 <103574@aluno.uricer.edu.br>
2025-09-16 20:55:20 -0300

    feat: database connection

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,24 @@
-const express = require('express');
-const app = express();
+const express = require("express")
+const { AppDataSource } = require("./src/config/database")
 
-app.get('/', (req, res) => {
-  res.send('Olá, mundo!');
-});
+const app = express()
 
-app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
+// Middleware para tratar JSON
+app.use(express.json())
+
+// Rota simples
+app.get("/", (req, res) => {
+  res.send("Olá, mundo!")
+})
+
+// Inicializar DB e servidor
+AppDataSource.initialize()
+  .then(() => {
+    console.log("📦 Database Connected")
+
+    const PORT = process.env.PORT || 3000
+    app.listen(PORT, () => {
+      console.log(`🚀 Server started on port ${PORT}`)
+    })
+  })
+  .catch((error) => console.error("❌ Error occurred: ", error))
---
+++ Backend/src/config/database.js
@@ -0,0 +1,15 @@
+require("reflect-metadata")
+const { DataSource } = require("typeorm")
+
+const AppDataSource = new DataSource({
+    type: "mysql",
+    host: "localhost",
+    port: 3307,
+    username: "nata",
+    password: "12345678",
+    database: "portal_b2b",
+    synchronize: true,
+    logging: true
+})
+
+module.exports = { AppDataSource }
