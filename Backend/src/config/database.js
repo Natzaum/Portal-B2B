b6f0cc80d4cda323fe69b9cad56008373ff5dbcb
Vítor Nodari <vitornodari6@gmail.com>
2025-09-17 16:27:16 -0300

    criacao controller categoria e entities na database

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -1,18 +1,23 @@
 require("reflect-metadata")
 const { DataSource } = require("typeorm")
 const Cliente = require("../models/cliente")
-
+const Carrinho = require("../models/carrinho")
+const Categoria = require("../models/categoria")
+const ItensCarrinho = require("../models/itensCarrinho")
+const ItensPedidos = require("../models/itensPedidos")
+const Pedidos = require("../models/pedidos")
+const Produto = require("../models/produto")
 const AppDataSource = new DataSource({
   type: "mysql",
   host: "localhost",
-  
+
   port: 3307,
   username: "nata",
   password: "12345678",
   database: "portal_b2b",
   synchronize: true,
   logging: true,
-  entities: [Cliente],
+  entities: [Cliente,Carrinho,Categoria,ItensCarrinho,ItensPedidos,Pedidos,Produto],
   migrations: [],
   subscribers: [],
 })
---
+++ Backend/src/controller/categoria.js
@@ -0,0 +1,2 @@
+const { AppDataSource } = require("../config/database")
+const Cliente = require("../models/categoria")
