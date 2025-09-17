7fc62012e44752ae295b998c7995f4fb03528f5f
Natzaum <natacezerbordignon@gmail.com>
2025-09-17 16:00:30 -0300

    feat: rotes

--- Backend/index.js
+++ Backend/index.js
@@ -6,7 +6,13 @@ const app = express()
 
 app.use(express.json())
 
+const { listarClientes, buscarCliente, atualizarCliente, removerCliente } = require("./src/controller/cliente")
+
 app.post("/clientes", registrarCliente)
+app.get("/clientes", listarClientes)
+app.get("/clientes/:id", buscarCliente)
+app.put("/clientes/:id", atualizarCliente)
+app.delete("/clientes/:id", removerCliente)
 
 app.get("/", (req, res) => {
   res.send("Olá, mundo!")
